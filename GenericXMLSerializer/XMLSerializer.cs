ffa4573c27e2e54ac6584a324d7ae6bda09a28d0
Liebeck <Liebeck@users.noreply.github.com>
2015-06-29 10:56:21 +0200

    Initial checkin

---
+++ GenericXMLSerializer/XMLSerializer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace GenericXMLSerializer
+{
+    public class XMLSerializer
+    {
+        public static void Serialize<T>(T data, String path) where T : class
+        {
+            Serialize<T>(data, path, "root");
+        }
+
+        public static void Serialize<T>(T data, String path, String xmlRootAttributeName) where T : class
+        {
+            using (FileStream stream = new FileStream(path, FileMode.Create))
+            {
+                XmlSerializer xmlSerializer = new XmlSerializer(typeof(T), new XmlRootAttribute(xmlRootAttributeName));
+                xmlSerializer.Serialize(stream, data);
+            }
+        }
+
+        public static T Deserialize<T>(String path, String xmlRootAttributeName) where T : class
+        {
+            using (FileStream stream = new FileStream(path, FileMode.Open))
+            {
+                XmlSerializer xmlSerializer = new XmlSerializer(typeof(T), new XmlRootAttribute(xmlRootAttributeName));
+                return xmlSerializer.Deserialize(stream) as T;
+            }
+        }
+
+        public static T Deserialize<T>(String path) where T : class
+        {
+            return Deserialize<T>(path, "root");
+        }
+    }
+}
